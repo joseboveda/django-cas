3b6e37312eb7faecc15f9e3119dd48656b46368c
Jose Boveda <jboveda@ucdavis.edu>
2013-03-20 14:23:01 -0700

    Added optional slashes to routes to work with additional get params

--- django_cas/urls.py
+++ django_cas/urls.py
@@ -1,11 +1,9 @@
 __author__ = 'sannies'
 
-from django.conf.urls.defaults import patterns, url
-
+from django.conf.urls import patterns, url
 
 urlpatterns = patterns('django_cas.views',
-    url(r'^login$', "login", name="cas_login"),
-    url(r'^logout$', "logout", name="cas_logout"),
-    url(r'^proxycallback$', "proxy_callback", name="cas_proxy_callback"),
-
+    url(r'^login/?$', "login", name="cas_login"),
+    url(r'^logout/?$', "logout", name="cas_logout"),
+    url(r'^proxycallback/?$', "proxy_callback", name="cas_proxy_callback"),
 )
